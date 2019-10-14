c454f8b59d5514f8f9edf2a833de40622c858386
Sergey Golov <air@sergeygolov.pp.ua>
2019-10-14 12:48:55 +0300

    task 1 2000 credits added

--- module-1/task-1.js
+++ module-1/task-1.js
@@ -1,13 +1,19 @@
 "use strict";
 
 const GOODS_NAME = "Генератор защитного поля";
-const GOODS_PRICE = 1000;
+let patternStringMessage
+let goodsPrice;
+goodsPrice = 1000;
 
 /* Вариант №1 конкатенции или сложение строк */
 const stringСoncatenationMessage =
-  "Выбран «" + GOODS_NAME + "», цена за штуку " + GOODS_PRICE + " кредитов";
+  "Выбран «" + GOODS_NAME + "», цена за штуку " + goodsPrice + " кредитов";
 console.log(stringСoncatenationMessage);
 
 /* Вариант №2 шаблонная строка */
-const patternStringMessage = `Выбран «${GOODS_NAME}», цена за штуку ${GOODS_PRICE} кредитов`;
+patternStringMessage = `Выбран «${GOODS_NAME}», цена за штуку ${goodsPrice} кредитов`;
+console.log(patternStringMessage);
+
+goodsPrice = 2000;
+patternStringMessage = `Выбран «${GOODS_NAME}», цена за штуку ${goodsPrice} кредитов`;
 console.log(patternStringMessage);
