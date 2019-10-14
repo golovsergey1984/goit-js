11a01417da627ded774e67489c810b93ad899f91
Sergey Golov <air@sergeygolov.pp.ua>
2019-10-14 12:07:14 +0300

    prettier task 5 added

--- module-1/task-5.js
+++ module-1/task-5.js
@@ -7,47 +7,56 @@ const USER_CHOICE = prompt(
   ""
 );
 switch (USER_CHOICE) {
-    case "":
+  case "":
     alert(
       'Вы не указали страну к доставке!\nУкажите страну заново или нажмите "Отмена" '
     );
     break;
     throw "stop";
 
-    case null:
-            alert("Отменено пользователем!");
-            break;    
-            throw "stop";
+  case null:
+    alert("Отменено пользователем!");
+    break;
+    throw "stop";
 }
 
-
-
-userCountryDelivery =  USER_CHOICE. toLowerCase();
-userCountryDelivery = userCountryDelivery[0]. toUpperCase() + userCountryDelivery.substring(1)
+userCountryDelivery = USER_CHOICE.toLowerCase();
+userCountryDelivery =
+  userCountryDelivery[0].toUpperCase() + userCountryDelivery.substring(1);
 switch (userCountryDelivery) {
-case 'Китай':
-cost=100;
-break;
+  case "Китай":
+    cost = 100;
+    break;
 
-case 'Чили':
-cost=250;
-break;
+  case "Чили":
+    cost = 250;
+    break;
 
-case 'Австралия':
-cost=170;
-break;
+  case "Австралия":
+    cost = 170;
+    break;
 
-case 'Индия':
-cost=80;
-break;
+  case "Индия":
+    cost = 80;
+    break;
 
-case 'Ямайка':
-cost=120;
-break;
+  case "Ямайка":
+    cost = 120;
+    break;
 
-default: 
-alert(`К сожалению, в указанную Вами страну: `+userCountryDelivery+ ` Мы доставки не совершаем :(`);
-break;
+  default:
+    alert(
+      `К сожалению, в указанную Вами страну: ` +
+        userCountryDelivery +
+        ` Мы доставки не совершаем :(`
+    );
+    break;
 }
 
-alert(`Досавка в ` +userCountryDelivery+ ` - возможна\nСтоимость составляет ` + cost+` кредитов`);
+alert(
+  `Досавка в ` +
+    userCountryDelivery +
+    ` - возможна\nСтоимость составляет ` +
+    cost +
+    ` кредитов`
+);
