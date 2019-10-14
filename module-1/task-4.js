312e093a1ea41631117c9d2640474f076c771dee
Sergey Golov <air@sergeygolov.pp.ua>
2019-10-14 13:36:07 +0300

    task 4 bugs fixed

--- module-1/task-4.js
+++ module-1/task-4.js
@@ -1,14 +1,36 @@
 "use strict";
 
 const USER_CREDITS = 23580;
-const ITEM_DROID = 3000;
+const PRICE_PER_DROID = 3000;
 
-const USER_DROIDS_CHOICE = prompt("Сколько дроидов Вы хотите купить", "");
-const TOTAL_PRICE = ITEM_DROID * USER_DROIDS_CHOICE;
+const USER_QUANTITY_SELECTION = prompt("Сколько дроидов Вы хотите купить", "");
+const TOTAL_PRICE = PRICE_PER_DROID * USER_QUANTITY_SELECTION;
 let userDifference;
 let userCash;
 
-switch (USER_DROIDS_CHOICE) {
+if (USER_QUANTITY_SELECTION === null) {
+  alert("Отменено пользователем!");
+} else if (USER_QUANTITY_SELECTION === "") {
+  alert("Вы не указали количество дроидов к покупке!");
+} else if (USER_QUANTITY_SELECTION === "0") {
+  alert("Вы укали 0 шт, заказ отклонен!! ");
+} else if (TOTAL_PRICE > USER_CREDITS) {
+  userDifference = USER_CREDITS - TOTAL_PRICE;
+  alert(
+    `Недостаточно средств на счету!\nДля совершения указанной покупки, Вам не хватает ` +
+      userDifference
+  );
+} else if (TOTAL_PRICE <= USER_CREDITS) {
+  userCash = USER_CREDITS - USER_QUANTITY_SELECTION * PRICE_PER_DROID;
+  alert(
+    `Поздравляем!\nВы купили ` +
+      USER_QUANTITY_SELECTION +
+      ` шт. дроидов!\nНа Вашем счету осталось ` +
+      userCash
+  );
+}
+
+/* switch (USER_QUANTITY_SELECTION) {
   case null:
     alert("Отменено пользователем!");
     break;
@@ -27,11 +49,11 @@ switch (USER_DROIDS_CHOICE) {
           userDifference
       );
       break;
-    } else userCash = USER_CREDITS - USER_DROIDS_CHOICE * ITEM_DROID;
+    } else userCash = USER_CREDITS -  USER_QUANTITY_SELECTION * PRICE_PER_DROID ;
     alert(
       `Поздравляем!\nВы купили ` +
         USER_DROIDS_CHOICE +
         ` шт. дроидов!\nНа Вашем счету осталось ` +
         userCash
     );
-}
+} */
