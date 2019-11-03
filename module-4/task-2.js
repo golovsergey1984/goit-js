aaceee4d50437559b08c15a0e4e1288cd1b72547
Sergey Golov <air@sergeygolov.pp.ua>
2019-11-03 15:10:32 +0200

    task 2 bugs moved

--- module-4/task-2.js
+++ module-4/task-2.js
@@ -1,4 +1,41 @@
 'use strict';
+
+const inventory = {
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+    this.items.push(itemName);
+  },
+
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+
+  action.call(this, itemName);
+};
+
+invokeInventoryAction.call(inventory, 'Аптечка', inventory.add);
+// Invoking action on Аптечка
+// Adding Аптечка to inventory
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryAction.call(inventory, 'Фильтр', inventory.remove);
+// Invoking action on Фильтр
+// Removing Фильтр from inventory
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
+
+/* 
+
+//Первый вариант с привязкой к объекту
+
 const inventory = {
   items: ['Монорельса', 'Фильтр'],
   add(itemName) {
@@ -30,4 +67,4 @@ invokeInventoryAction('Фильтр', inventory.remove);
 // Invoking action on Фильтр
 // Removing Фильтр from inventory
 
-console.log(inventory.items); // ['Монорельса', 'Аптечка']
+console.log(inventory.items); // ['Монорельса', 'Аптечка'] */
