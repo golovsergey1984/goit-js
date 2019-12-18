8bb36036b6e20c2b6fbcbb6bdc91d27d3040cf32
Sergey Golov <air@sergeygolov.pp.ua>
2019-12-18 13:54:34 +0200

    bug fixed

--- HW-10/src/change-theme.js
+++ HW-10/src/change-theme.js
@@ -1,4 +1,4 @@
-const Theme = {
+const bgColor = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
@@ -8,20 +8,45 @@ const refs = {
   body: document.querySelector('body'),
   switch: document.getElementById('theme-switch-control'),
 };
-refs.switch.addEventListener('click', choiceMade);
 
-function choiceMade(event) {
+const saveDataInput = localStorage.getItem('check');
+const saveDataTheme = localStorage.getItem('theme');
+
+if (saveDataInput == 'true') {
+  refs.body.setAttribute('class', saveDataTheme);
+  refs.switch.setAttribute('checked', saveDataInput);
+}
+
+refs.switch.addEventListener('click', choiceMade => {
   if (refs.switch.checked) {
-    refs.body.classList.add(Theme.DARK);
+    refs.body.classList.add(bgColor.DARK);
     refs.switch.setAttribute('checked', 'true');
-    localStorage.setItem(refs.switch, 'true');
   } else {
-    refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    refs.body.classList.replace(bgColor.DARK, bgColor.LIGHT);
     refs.switch.setAttribute('checked', 'false');
-    localStorage.setItem(refs.switch, 'false');
+  }
+  const theme = refs.body.getAttribute('class');
+  localStorage.setItem('theme', theme);
+
+  const input = choiceMade.currentTarget.attributes.checked;
+  localStorage.setItem('check', input.value);
+});
+
+/* 
+1st var
+function choiceMade(event) { 
+ if (refs.switch.checked) {
+    refs.body.classList.add(bgColor.DARK);
+    refs.switch.setAttribute('checked', 'true');
+
+    localStorage.setItem('refs.switch', 'true'); 
+  } else {
+    refs.body.classList.replace(bgColor.DARK, bgColor.LIGHT);
+    refs.switch.setAttribute('checked', 'false');
+    localStorage.setItem(refs.switch, 'false'); 
   }
 }
-if (localStorage.getItem(refs.switch) == 'true') {
+ if (localStorage.getItem(refs.switch) == 'true') {
   refs.body.classList.add(Theme.DARK);
   refs.switch.setAttribute('checked', 'true');
-}
+}  */
