d7a4c2053753b9e89c0bf57a4006bd073349905b
Sergey Golov <air@sergeygolov.pp.ua>
2019-12-28 13:15:18 +0200

    HW12 finish

--- HW-12/countries/src/js/countries.js
+++ HW-12/countries/src/js/countries.js
@@ -8,6 +8,7 @@ import PNotifyStyleMaterial from 'pnotify/dist/es/PNotifyStyleMaterial.js';
 import PNotify from 'pnotify/dist/es/PNotify';
 PNotify.defaults.styling = 'material';
 PNotify.defaults.icons = 'material';
+var debounce = require('../../node_modules/lodash');
 
 const refs = {
   countryChoice: document.getElementById('country-input'),
@@ -16,9 +17,53 @@ const refs = {
   countryCard: document.getElementById('country-item'),
 };
 
-refs.countryChoice.addEventListener('input', searchCountry);
+let debounced = _.debounce(searchCountry, 500);
+
+refs.countryChoice.addEventListener('input', debounced);
 
 function searchCountry(e) {
+  clearSearchList();
+  e.preventDefault();
+
+  const searchQuery = e.originalTarget.value;
+
+  countryService.fetchCountry(searchQuery).then(data => {
+    console.log(data);
+    if (data.length > 10) {
+      PNotify.error({
+        text: 'Too many matches found. Please, enter a more specfic query!',
+      });
+    }
+    if (data.length === 1) {
+      const markup = buildItemCard(data);
+      insertSearchList(markup);
+      const a = document.getElementById('country-result').data;
+      searchQuery = '';
+    }
+    if (data.length >= 1 && data.length <= 10) {
+      const markup = buildSearchList(data);
+      insertSearchList(markup);
+    }
+  });
+}
+
+function clearSearchList() {
+  refs.inputResult.innerHTML = '';
+}
+
+function insertSearchList(markup) {
+  refs.inputResult.insertAdjacentHTML('beforeend', markup);
+}
+
+function buildSearchList(items) {
+  return restResultTemplate(items);
+}
+
+function buildItemCard(items) {
+  return restResultCardTemplate(items);
+}
+
+/* function searchCountry(e) {
   clearSearchList();
   e.preventDefault();
 
@@ -59,7 +104,9 @@ function buildItemCard(items) {
   return restResultCardTemplate(items);
 }
 
-/* const refs = {
+/* 
+1й Вариант
+const refs = {
   countryChoice: document.getElementById('country-input'),
   baseUrl: 'https://restcountries.eu/rest/v2',
   resource: '/name/',
