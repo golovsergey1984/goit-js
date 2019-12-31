fab8df07c22595a9ef9b5f1b573a3714f802ce99
Sergey Golov <air@sergeygolov.pp.ua>
2019-12-31 13:40:53 +0200

    13:40 finish

--- HW-12/countries/src/js/services/country-service.js
+++ HW-12/countries/src/js/services/country-service.js
@@ -3,16 +3,17 @@ const resource = '/name/';
 
 export default {
   fetchCountry(searchQuery) {
-    return fetch(baseUrl + resource + searchQuery).then(function(response) {
-      if (response.ok) {
-        return response.json();
-      } else {
-        let data = [];
-        return data;
-      }
-    });
-    /*       .catch(error => {
-        return '';
-      }); */
+    return fetch(baseUrl + resource + searchQuery)
+      .then(function(response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          let data = [];
+          return data;
+        }
+      })
+      .catch(error => {
+        return;
+      });
   },
 };
