4beeeb571ba4f95631920c5691e5c3026abae7c9
Sergey Golov <air@sergeygolov.pp.ua>
2020-01-11 13:44:48 +0200

    per_page=12 added

--- HW-13/image-finder/src/api/images.api.js
+++ HW-13/image-finder/src/api/images.api.js
@@ -1,12 +1,15 @@
 /* 'https://pixabay.com/api/?key=14759738-915ddabe7286f1d5b09b52b7a&q=yellow+flowers&image_type=photo' */
+/* https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ */
 
 const baseUrl = 'https://pixabay.com/api/?key=';
 export default {
   page: 1,
+
   query: '',
   fetchImages() {
     const API_KEY = '14759738-915ddabe7286f1d5b09b52b7a';
-    const requestParams = '&q=' + this.query + '&page=' + this.page;
+    const requestParams =
+      '&q=' + this.query + '&page=' + this.page + '&per_page=12';
 
     return fetch(baseUrl + API_KEY + requestParams).then(response =>
       response.json().then(parsedResponse => {
